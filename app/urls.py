23c6a8e1925b8b39b816c34de232ab74e72b9d41
PsyShadowF <felipecarvalho5520@gmail.com>
2023-05-14 21:15:52 -0300

    Páginas Home, Membros e esqueletoda Artigo feitas

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('membros/', views.members, name='members'),
+    path('artigos/', views.articles, name='articles'),
+    path('projetos/', views.projects, name='projects'),
+    path('contato/', views.contact, name='contact'),
 ]
--- app/views.py
+++ app/views.py
@@ -5,3 +5,19 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+
+def members(request):
+    return render(request, 'members.html')
+
+
+def articles(request):
+    return render(request, 'articles.html')
+
+
+def projects(request):
+    return render(request, 'projects.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
